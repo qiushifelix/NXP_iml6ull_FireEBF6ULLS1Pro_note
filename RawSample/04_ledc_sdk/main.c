fc4091fbd2da1023be419b5b095053d6c43a796e
fuqiushi <404363872@qq.com>
2024-07-17 14:37:44 +0800

    Adapt to Ebf6ull S1 Pro Development Board modify Makefile 适配野火开发板 修改makefile

--- RawSample/04_ledc_sdk/main.c
+++ RawSample/04_ledc_sdk/main.c
@@ -46,7 +46,7 @@ void clk_enable(void)
 void led_init(void)
 {
 	/* 1、初始化IO复用 */
-	IOMUXC_SetPinMux(IOMUXC_GPIO1_IO03_GPIO1_IO03,0);		/* 复用为GPIO1_IO0 */
+	IOMUXC_SetPinMux(IOMUXC_GPIO1_IO04_GPIO1_IO04,0);		/* 复用为GPIO1_IO0 */
 
 	/* 2、、配置GPIO1_IO03的IO属性	
 	 *bit 16:0 HYS关闭
@@ -58,13 +58,13 @@ void led_init(void)
      *bit [5:3]: 110 R0/6驱动能力
      *bit [0]: 0 低转换率
      */
-	IOMUXC_SetPinConfig(IOMUXC_GPIO1_IO03_GPIO1_IO03,0X10B0);
+	IOMUXC_SetPinConfig(IOMUXC_GPIO1_IO04_GPIO1_IO04,0X10B0);
 
 	/* 3、初始化GPIO,设置GPIO1_IO03设置为输出  */
-	GPIO1->GDIR |= (1 << 3);	
+	GPIO1->GDIR |= (1 << 4);	
 	
 	/* 4、设置GPIO1_IO03输出低电平，打开LED0 */
-	GPIO1->DR &= ~(1 << 3);			
+	GPIO1->DR &= ~(1 << 4);			
 }
 
 /*
@@ -75,7 +75,7 @@ void led_init(void)
 void led_on(void)
 {
 	/* 将GPIO1_DR的bit3清零 	*/
-	GPIO1->DR &= ~(1<<3); 
+	GPIO1->DR &= ~(1<<4); 
 }
 
 /*
@@ -86,7 +86,7 @@ void led_on(void)
 void led_off(void)
 {
 	/* 将GPIO1_DR的bit3置1 */
-	GPIO1->DR |= (1<<3); 
+	GPIO1->DR |= (1<<4); 
 }
 
 /*
@@ -126,10 +126,10 @@ int main(void)
 	while(1)			/* 死循环 				*/
 	{	
 		led_off();		/* 关闭LED 			*/
-		delay(500);		/* 延时500ms 			*/
+		delay(200);		/* 延时500ms 			*/
 
 		led_on();		/* 打开LED 			*/
-		delay(500);		/* 延时500ms 			*/
+		delay(200);		/* 延时500ms 			*/
 	}
 
 	return 0;
